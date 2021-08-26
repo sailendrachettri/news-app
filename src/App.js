6b0cf66ad1084baf72c8e6400b11ab75dd62bfa1
sailendrachettri <sailendra9083@gmail.com>
2021-08-26 15:23:53 +0530

    navbar added - #HarryBhai

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
     c = 'John';
     render() {
         return (
             <div>
-                <h1>Hello my first class based Component{this.c}</h1>
+                <Navbar />
+                <News />
             </div>
         )
     }
---
+++ src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        return (
+            <>
+                <h3>This is a news item</h3>
+            </>
+        )
+    }
+}
+
+export default NewsItem
