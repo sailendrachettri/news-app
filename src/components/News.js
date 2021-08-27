9f7a23ca52114e97cb0402f5bc57a0966b253d1c
sailendrachettri <sailendra9083@gmail.com>
2021-08-27 13:09:41 +0530

    Fetching API Key from News API

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                <h1>This is a news component</h1>
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
+            <div className="container my-3">
+                <h2>NewsMonkey - Top Headlines</h2>
+                <div className="row">
+                    <div className="col md-4">
+                        <NewsItem title="Mytitle" description="this is description" />
+                    </div>
+                    <div className="col md-4">
+                        <NewsItem title="Mytitle" description="this is description" />
+                    </div>
+                    <div className="col md-4">
+                        <NewsItem title="Mytitle" description="this is description" />
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class NewsItem extends Component {
     render() {
+       let {title, description} = this.props;
         return (
             <>
-                <h3>This is a news item</h3>
+                <div className="card" style={{width: "18rem"}}>
+                    <img src="https://static.politico.com/45/56/0a779f9346b792b3338f03efc3ad/210310-capitol-riot-773.jpg" className="card-img-top" alt="news" />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href="/" className="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
             </>
         )
     }
